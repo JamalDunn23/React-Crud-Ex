2a7ba539935458f7e158b97520133231063361e9
JamalDunn23 <jamaldunn23@gmail.com>
2023-08-11 12:04:50 -0400

    React App Started, Boiler Plate Code Removed, Install react router

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <Router>
+
+      <h1>ScreenViews</h1>
+
+      </Router>
+    </>
+  )
+}
+
+export default App
